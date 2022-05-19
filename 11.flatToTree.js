4294942491dc6514f3ce9aadd1b81eb2e8d3fd6a
LiuYang <a18629092384@gmail.com>
2022-05-19 15:29:17 +0800

    tree=>list

--- 11.flatToTree.js
+++ 11.flatToTree.js
@@ -26,3 +26,21 @@ function arrToTree(arr) {
 }
 
 console.log(arrToTree(arra))
+
+
+// tree转化为列表
+let tree = '[{"id":1,"name":"部门1","pid":0,"children":[{"id":2,"name":"部门2","pid":1,"children":[]},{"id":3,"name":"部门3","pid":1,"children":[{"id":4,"name":"部门4","pid":3,"children":[{"id":5,"name":"部门5","pid":4,"children":[]}]}]}]}]'
+tree = JSON.parse(tree);
+
+function treeToFlat(arr) {
+	let treeArray = [];
+	function recursive(arr) {
+		if (arr.length === 0) return;
+		arr.forEach(item => {
+			treeArray.push(item)
+			recursive(item.children)
+		})
+	}
+	recursive(tree)
+	return treeArray;
+}
