02b2b295474b0f73958acedf1c0297331f888b73
LiuYang <a18629092384@gmail.com>
2023-01-13 10:41:01 +0800

    leetcode daily

--- leetcode-daily.js
+++ leetcode-daily.js
@@ -70,5 +70,56 @@ var search = function(nums, target) {
  * @return {string}
  */
 var addBinary = function(a, b) {
-	
+	let aa = a.split('').reverse();
+	let bb = b.split('').reverse();
+	let len = Math.max(a.length, b.length);
+	let pre = 0;
+	let result = [];
+	for (let i = 0; i < len; i++) {
+		const an = aa[i] && Number(aa[i]) || 0;
+		const bn = bb[i] && Number(bb[i]) || 0;
+		result.push((an + bn + pre) % 2);
+		if (an + bn + pre > 1) {
+			pre = 1;
+			if (i === len - 1) {
+				result.push(1);
+			}
+		} else {
+			pre = 0;
+		}
+	}
+	return result.reverse().join("")
+};
+// console.log(addBinary("11", "1"));
+
+/**
+ * 2287. 重排字符形成目标字符串
+ * @param {string} s
+ * @param {string} target
+ * @return {number}
+ */
+var rearrangeCharacters = function(s, target) {
+	let dic = {};
+	for (let i = 0; i < s.length; i++) {
+		if (target.includes(s[i])) {
+			dic[s[i]] && dic[s[i]]++ || (dic[s[i]] = 1)
+		}
+	}
+	function getOne() {
+		let flag = true
+		for (let i = 0; i < target.length; i++) {
+			dic[target[i]] && dic[target[i]]-- || (flag = false)
+		}
+		return flag;
+	}
+	let hasMore = true;
+	let count = 0;
+	while (hasMore) {
+		if (getOne()) {
+			count++
+		} else {
+			hasMore = false;
+		}
+	}
+	return count
 };
