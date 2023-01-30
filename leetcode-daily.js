d32399adcc058df266c249fa6a39473111ed7445
LiuYang <a18629092384@gmail.com>
2023-01-30 11:22:26 +0800

    todo

--- leetcode-daily.js
+++ leetcode-daily.js
@@ -259,7 +259,7 @@ var simplifyPath = function(path) {
 // console.log(res);
 
 /**
- * 72. Edit Distance
+ * TODO 72. Edit Distance
  * Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
  * @param {string} word1
  * @param {string} word2
