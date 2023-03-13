7f11abf275d7a1c3b2361bf6903ce2b696a52474
LiuYang <liuyang@vchangyi.com>
2023-03-13 09:45:33 +0800

    formart

--- leetcode-daily.js
+++ leetcode-daily.js
@@ -731,6 +731,7 @@ var minimumRecolors = function (blocks, k) {
 
 
 /**
+ * 自动机
  * @param {string} s
  * @return {number}
  */
@@ -792,29 +793,6 @@ var myAtoi = function (s) {
 };
 
 //myAtoi('    words and 987')
-
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-var addTwoNumbers = function (l1, l2) {
-  while (l1 || l2) {
-    const currSum = l1.val + l2.val
-    if (dfsdf) {
-      
-    }
-  }
-};
-
-
 
 
 
