690214129dceed88b10631d7918889402e7a10ec
LiuYang <liuyang@vchangyi.com>
2023-05-07 10:46:14 +0800

    leetcode daily

--- leetcode-daily.js
+++ leetcode-daily.js
@@ -1582,5 +1582,19 @@ var hardestWorker = function(n, logs) {
   return ans
 };
 
-
+/**
+* @param {number[]} time
+* @return {number}
+*/
+var numPairsDivisibleBy60 = function(time) {
+  let ans = 0;
+  for (let i = 0; i < time.length; i++) {
+    for (let j = i + 1; j < time.length; j++) {
+      if ((time[i] + time[j]) % 60 === 0) {
+        ans++;
+      }
+    }
+  }
+  return ans;
+};
 
