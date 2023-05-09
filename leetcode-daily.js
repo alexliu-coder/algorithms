df7f820e38026a8a42e3dd29b6a661d3d83b72a0
LiuYang <liuyang@vchangyi.com>
2023-05-09 17:55:39 +0800

    leetcode

--- leetcode-daily.js
+++ leetcode-daily.js
@@ -1598,3 +1598,56 @@ var numPairsDivisibleBy60 = function(time) {
   return ans;
 };
 
+/**
+* @param {string} croakOfFrogs
+* @return {number}
+*/
+var minNumberOfFrogs = function(croakOfFrogs) {
+
+};
+
+/**
+* @param {string} time
+* @return {number}
+*/
+var countTime = function(time) {
+  let h = 0;
+  if (time[0] === '?' && time[1] === '?') {
+    h = 24;
+  } else if (time[0] === '?') {
+    if(Number(time[1]) < 4) {
+      h = 3;
+    } else {
+      h = 2;
+    }
+  } else if (time[1] === '?') {
+    if (Number(time[0]) < 2) {
+      h = 10;
+    } else {
+      h = 4;
+    }
+  } else {
+    h = 1;
+  }
+
+  let m = 0;
+  if (time[3] === '?' && time[4] === '?') {
+    m = 60;
+  } else if (time[3] === '?') {
+    m = 6;
+  } else if (time[4] === '?') {
+    m = 10;
+  } else {
+    m = 1
+  }
+  return m * h
+};
+
+/**
+* @param {number} n
+* @param {number} k
+* @return {number[][]}
+*/
+var combine = function(n, k) {
+
+};
